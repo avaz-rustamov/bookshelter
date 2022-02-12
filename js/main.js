f23285e50818b9f8a42a35cb0dd64dbd2739245f
avaz-rustamov <avaz.dev@icloud.com>
2022-02-13 02:32:35 +0500

    Add some code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-'use strict';
+"use strict";
+const elSiteGrid = document.querySelector(".site-grid");
+
+const renderBook = function (date) {
+  const html = `
+
+    <div class="site-grid__wrapper">
+      <div class="site-grid__img--wrapper">
+        <img src="" alt="" />
+      </div>
+      <div class="site-grid__heading"></div>
+      <p class="site-grid__text"></p>
+      <p class="site-grid__text--date"></p>
+      <div class="site-gird__footer--bookmark">
+        <button class="site-grid__footer--button--bookmark"></button>
+        <button class="site-grid__footer--button"></button>
+      </div>
+      <a href="" class="site-gird__link"></a>
+    </div>
+
+  `;
+  elSiteGrid.insertAdjacentHTML("beforeend", html);
+};
+
+const getBookDate = function (book) {
+  const requst = fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}`)
+    .then((res) => res.json())
+    .then((date) => date);
+  return requst;
+};
+
+console.log(getBookDate("python"));
