3912b74a4f60d44ed8f5ec7c2c7b5def54d59a4b
avaz-rustamov <avaz.dev@icloud.com>
2022-02-13 18:01:23 +0500

    Add info about sections

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 "use strict";
-const localToken = window.localStorage.getItem("token");
-if (!localToken) {
-  window.location.replace("login.html");
-}
 const elSiteGrid = document.querySelector(".site-grid");
-
 const elLogoutBtn = document.querySelector(".site-btn__logout");
 elLogoutBtn.addEventListener("click", function () {
   window.localStorage.removeItem("token");
   window.location.replace("login.html");
 });
 
+// ERROR MASSAGE
+const renderError = function (errorMassage) {
+  elSiteGrid.insertAdjacentText("beforeend", errorMassage);
+};
+
+// LOGIN
+const localToken = window.localStorage.getItem("token");
+if (!localToken) {
+  window.location.replace("login.html");
+}
+
+// RENDER BOOKS
 const renderBook = function (date) {
-  const books = date.items;
-  books.forEach((book) => {
-    const html = `
+  try {
+    const books = date.items;
+    books.forEach((book) => {
+      const html = `
 
     <div class="site-grid__wrapper">
       <div class="site-grid__img--wrapper">
@@ -22,19 +30,23 @@ const renderBook = function (date) {
       </div>
       <div class="site-grid__heading">${book.volumeInfo.title}</div>
       <p class="site-grid__text">${book.authors}</p>
-      <p class="site-grid__text--date">${book.publishedDate}</p>
+      <p class="site-grid__text--date">${book.volumeInfo.publishedDate}</p>
       <div class="site-gird__footer--bookmark">
         <button class="site-grid__footer--button--bookmark">Bookmar</button>
         <button class="site-grid__footer--button">More Info</button>
       </div>
-      <a href="${book.previewLink}" class="site-gird__link">Read</a>
+      <a href="${book.volumeInfo.previewLink}" class="site-gird__link" target="_blank">Read</a>
     </div>
 
   `;
-    elSiteGrid.insertAdjacentHTML("beforeend", html);
-  });
+      elSiteGrid.insertAdjacentHTML("beforeend", html);
+    });
+  } catch (err) {
+    renderError(err.message);
+  }
 };
 
+// FETCH DATE
 const getBookDate = function (book) {
   const requst = fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}`)
     .then((res) => res.json())
